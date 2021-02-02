7609cd73860d3498e65ea81a75c14da492efe9a5
Mr.Bhanuka Bandara <78149476+mrbhanuka-official@users.noreply.github.com>
2021-02-02 12:03:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyttsx3
+import PyPDF2
+book = open('java.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+fullpage = str(pages)
+
+print('This Book Contataint '+fullpage+ ' Number of Pages')
+
+speaker = pyttsx3.init()
+for num in range(21, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    nowpage = str(num)
+    print('Now I am Reading '+nowpage+' Page.')
+    speaker.say(text)
+    speaker.runAndWait()
